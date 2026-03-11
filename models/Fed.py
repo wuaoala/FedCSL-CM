f8fe02285ec0e4423a6e13cd211d50d9f4e492c3
wuaoala <72646410+wuaoala@users.noreply.github.com>
2026-03-11 11:06:03 +0800

    Update Fed.py

--- models/Fed.py
+++ models/Fed.py
@@ -14,10 +14,18 @@ def decrypt_vector(private_key, parameters):
     parameters = [private_key.decrypt(parameter) for parameter in parameters]
     return parameters
 
+def FedAvg(w):
+    w_avg = copy.deepcopy(w[0])
+    for k in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[k] += w[i][k]
+        w_avg[k] = torch.div(w_avg[k], len(w))
+    return w_avg
+
 def FedSec_Cloud(w,local_w,public_key):
     for i in range(len(w)):
         for k in w[i].keys():
-            w[i][k] = w[i][k]*local_w[i]
+            w[i][k] = w[i][k] * local_w[i]
     w_locals = w
     # 加密局部模型参数进行安全聚合
     w_shape = {}
@@ -34,4 +42,18 @@ def FedSec_Cloud(w,local_w,public_key):
                 sum_parameters[key] = np.add(sum_parameters[key], encrypt_vector(public_key, w_locals[i][key]))
     encrypt_w_glob = sum_parameters
     return encrypt_w_glob, w_shape
-
+def FedNova(w):
+    w_avg = copy.deepcopy(w[0])
+    w_avg_para_norm = torch.norm(torch.cat([x.view(-1) for x in w_avg.values()]))
+    for k in w_avg.keys():
+        w_avg[k] = torch.div(w_avg[k], w_avg_para_norm)
+        for i in range(1, len(w)):
+            para_norm = torch.norm(torch.cat([x.view(-1) for x in w[i].values()]))
+            w_avg[k] += torch.div(w[i][k], para_norm)
+    return w_avg
+def SCAFFOLD(C):
+    c_avg = C[0]
+    for i in range(1, len(C)):
+        c_avg += C[i]
+    c_avg = [torch.div(i,len(C)) for i in c_avg]
+    return c_avg
