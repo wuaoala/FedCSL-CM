ec25f7e442b3b2c75ea7425648c8e1dd037324cb
wuaoala <72646410+wuaoala@users.noreply.github.com>
2026-03-30 11:19:31 +0800

    Update local_learning_methods.py

--- local_learning_methods.py
+++ local_learning_methods.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     # parse args
     args = args_parser()
     args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
-    args.dataset = 'LC'
+    args.dataset = 'A'
     args.seed = 11
     torch.manual_seed(11)
 
